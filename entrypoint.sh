af057ea37ae42710bb3829688fc0be5d21b498d3
Braxton Beyer <bbeyer@infoblox.com>
2021-06-28 13:21:31 -0500

    update to use v2 syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,23 @@ if [ -z ${INPUT_CFN_DIRECTORY} ] ; then
     exit 1
 fi
 
+if [ -z ${INPUT_RULESET_FILE} ] ; then
+    echo "Missing 'ruleset_file' parameter."
+    echo "Set this to the path where your cfn-guard rules are located."
+    exit 1
+fi
+
 # find templates with resource
-POSSIBLE_TEMPLATES=`grep --with-filename --recursive 'Resources' ${INPUT_CFN_DIRECTORY}/* | cut -d':' -f1 | sort -u`
+POSSIBLE_TEMPLATES=`grep --with-filename --recursive 'Resources' ${INPUT_CFN_DIRECTORY}/* | cut -d':' -f1 | sort -u` for f in $POSSIBLE_TEMPLATES; do
 
-for f in $POSSIBLE_TEMPLATES; do
+    # TODO: check for rules file and either use that for all or look for mathcing tmeplate name
     # echo "Checking for ruleset matching template file: ${f}"
-
     # rules=${f%.*}.ruleset
     # ruleset_file=ruleset_file
+
     if [ -e $INPUT_RULESET_FILE ]; then
         echo "                                      Found: $rules"
-        cg_cmd="cfn-guard check --strict-checks --rule_set $INPUT_RULESET_FILE  --template ${PWD}/${f}"
+        cg_cmd="cfn-guard validate --rules $INPUT_RULESET_FILE --data ${PWD}/${f}"
         echo "Running command:"
         echo "$ $cg_cmd"
         $cg_cmd
