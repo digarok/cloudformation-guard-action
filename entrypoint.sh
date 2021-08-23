9d75dd0a0708365eeb82bb4a0a2baf3deacfbc80
Braxton Beyer <bbeyer@infoblox.com>
2021-08-23 18:00:14 -0500

    fix grep hopefully

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,22 +15,23 @@ echo "INPUT_RULESET_FILE set to $INPUT_RULESET_FILE"
 
 # find templates with resource
 POSSIBLE_TEMPLATES=$(grep --with-filename --recursive 'Resources' ${INPUT_CFN_DIRECTORY}/* | cut -d':' -f1 | sort -u)
-for f in $POSSIBLE_TEMPLATES; do
+for F in $POSSIBLE_TEMPLATES; do
   # TODO: check for rules file and either use that for all or look for mathcing tmeplate name
   # echo "Checking for ruleset matching template file: ${f}"
   # rules=${f%.*}.ruleset
   # ruleset_file=ruleset_file
 
   echo ">>>>>"
-  echo "evaluating $f"
+  echo "evaluating $F"
 
   # if [ "$f" != "$INPUT_RULESET_FILE" ] # dont scan our own ruleset file
   # if ! [ echo $f | grep ruleset ] # dont scan ruleset files
-  if [[ "$f" == *"ruleset"* ]]
+  # if [[ "$F" == *"ruleset"* ]]
+  if grep -q "ruleset" <<< "$F"
   then
     echo "Skipping ruleset file"
   else
-    cg_cmd="cfn-guard validate --rules $INPUT_RULESET_FILE --data ${PWD}/${f} --type CFNTemplate"
+    cg_cmd="cfn-guard validate --rules $INPUT_RULESET_FILE --data ${PWD}/${F} --type CFNTemplate"
     $cg_cmd
 
     if [ $? -ne 0 ]
