f4f0fec0116f483a214be9e9dba3fb6b999d6ecc
Braxton Beyer <bbeyer@infoblox.com>
2021-08-23 17:48:06 -0500

    fix linting issues and update if command again

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # validate inputs
 
-if [ -z ${INPUT_CFN_DIRECTORY} ] ; then
+if [ -z "${INPUT_CFN_DIRECTORY}" ] ; then
   echo "Missing 'cfn_directory' parameter."
   echo "Set this to the directory where your CloudFormation Templates are located."
   exit 1
@@ -14,7 +14,7 @@ echo "INPUT_RULESET_FILE set to $INPUT_RULESET_FILE"
 # cat $INPUT_RULESET_FILE
 
 # find templates with resource
-POSSIBLE_TEMPLATES=`grep --with-filename --recursive 'Resources' ${INPUT_CFN_DIRECTORY}/* | cut -d':' -f1 | sort -u`
+POSSIBLE_TEMPLATES=$(grep --with-filename --recursive 'Resources' "${INPUT_CFN_DIRECTORY}/*" | cut -d':' -f1 | sort -u)
 for f in $POSSIBLE_TEMPLATES; do
   # TODO: check for rules file and either use that for all or look for mathcing tmeplate name
   # echo "Checking for ruleset matching template file: ${f}"
@@ -27,8 +27,11 @@ for f in $POSSIBLE_TEMPLATES; do
   # if [ -e $INPUT_RULESET_FILE ]; then
 
   # if [ "$f" != "$INPUT_RULESET_FILE" ] # dont scan our own ruleset file
-  if ! [ echo $f | grep ruleset ] # dont scan ruleset files
+  # if ! [ echo $f | grep ruleset ] # dont scan ruleset files
+  if [ "$f" == *"ruleset"* ]
   then
+      echo "Skipping ruleset file"
+  else
     cg_cmd="cfn-guard validate --rules $INPUT_RULESET_FILE --data ${PWD}/${f} --type CFNTemplate"
     $cg_cmd
 
