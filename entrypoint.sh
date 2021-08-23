91ebf82613e7e51b1ae13474220ee9d247e5075f
Braxton Beyer <bbeyer@infoblox.com>
2021-08-23 13:17:07 -0500

    adding some output for easier reading of results

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,10 @@ if [ -z ${INPUT_CFN_DIRECTORY} ] ; then
   exit 1
 fi
 
-if [ -z ${INPUT_RULESET_FILE} ] ; then
-  echo "No 'ruleset_file' parameter passed. Using default."
-  echo "Set this to the path where your cfn-guard rules are located."
-  # exit 1
-fi
-
-# INPUT_RULESET_FILE="${INPUT_RULESET_FILE:-'/cfn-guard.ruleset'}"  # If variable not set or null, use default.
-
+# check if using default or provided rules
 echo "INPUT_RULESET_FILE set to $INPUT_RULESET_FILE"
 
-cat $INPUT_RULESET_FILE
+# cat $INPUT_RULESET_FILE
 
 # find templates with resource
 POSSIBLE_TEMPLATES=`grep --with-filename --recursive 'Resources' ${INPUT_CFN_DIRECTORY}/* | cut -d':' -f1 | sort -u`
@@ -28,6 +21,7 @@ for f in $POSSIBLE_TEMPLATES; do
   # rules=${f%.*}.ruleset
   # ruleset_file=ruleset_file
 
+  echo ">>>>>"
   if [ -e $INPUT_RULESET_FILE ]; then
     if [ "$f" != "$INPUT_RULESET_FILE" ] # dont scan our own ruleset file
     then
@@ -43,6 +37,9 @@ for f in $POSSIBLE_TEMPLATES; do
   else
     echo "No matching: $rules"
   fi
+
+  echo "<<<<<"
+  echo ""
 done
 
 echo "CloudFormation Guard Scan Complete"
