b8639e439b615a953e160216da8373f1ef363db5
Braxton Beyer <bbeyer@infoblox.com>
2021-08-24 10:29:49 -0500

    more cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # validate inputs
-
 if [ -z "${INPUT_CFN_DIRECTORY}" ] ; then
   echo "Missing 'cfn_directory' parameter."
   echo "Set this to the directory where your CloudFormation Templates are located."
@@ -11,22 +10,12 @@ fi
 # check if using default or provided rules
 echo "INPUT_RULESET_FILE set to $INPUT_RULESET_FILE"
 
-# cat $INPUT_RULESET_FILE
-
-# find templates with resource
+# find templates with 'Resources'
 POSSIBLE_TEMPLATES=$(grep --with-filename --recursive 'Resources' ${INPUT_CFN_DIRECTORY}/* | cut -d':' -f1 | sort -u)
-for F in $POSSIBLE_TEMPLATES; do
-  # TODO: check for rules file and either use that for all or look for mathcing tmeplate name
-  # echo "Checking for ruleset matching template file: ${f}"
-  # rules=${f%.*}.ruleset
-  # ruleset_file=ruleset_file
 
+for F in $POSSIBLE_TEMPLATES; do
   echo "START evaluating $F"
 
-  # if [ "$f" != "$INPUT_RULESET_FILE" ] # dont scan our own ruleset file
-  # if [[ "$F" == *"ruleset"* ]]
-  # if grep -q "ruleset" <<< "$F"
-  # if [ "$F" =~ ".+ruleset.+" ] # dont scan our own ruleset file
   if echo "$F" | grep ruleset
   then
     echo "Skipping ruleset file"
